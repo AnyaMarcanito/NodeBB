e51127263601c60df92ea2467219248d1f3347a6
AnyaMarcanito <19-10336@usb.ve>
2024-10-14 04:25:56 -0400

    Refactor watch.ts and fixing eslint rules problems with any types

--- src/categories/watch.ts
+++ src/categories/watch.ts
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-require-imports */
 
-const db = require('../database');
-const user = require('../user');
+import db from '../database';
+import user from '../user';
 
 interface WatchStates {
     ignoring: number;
@@ -28,68 +24,83 @@ interface CategoriesType {
     getUidsWatchStates: (cid: number, uids: number[]) => Promise<number[]>;
 }
 
-module.exports = function (Categories: CategoriesType) {
-	Categories.watchStates = {
+const Categories: CategoriesType = {
+	watchStates: {
 		ignoring: 1,
 		notwatching: 2,
 		tracking: 3,
 		watching: 4,
-	};
+	},
 
-	Categories.isIgnored = async function (cids: number[], uid: number): Promise<boolean[]> {
+	isIgnored: async function (this: CategoriesType, cids: number[], uid: number): Promise<boolean[]> {
 		if (!(parseInt(uid.toString(), 10) > 0)) {
 			return cids.map(() => false);
 		}
-		const states = await Categories.getWatchState(cids, uid);
-		return states.map(state => state === Categories.watchStates.ignoring);
-	};
+		const states: number[] = await this.getWatchState(cids, uid);
+		return states.map(state => state === this.watchStates.ignoring);
+	},
 
-	Categories.getWatchState = async function (cids: number[], uid: number): Promise<number[]> {
+	getWatchState: async function (this: CategoriesType, cids: number[], uid: number): Promise<number[]> {
 		if (!(parseInt(uid.toString(), 10) > 0)) {
-			return cids.map(() => Categories.watchStates.notwatching);
+			return cids.map(() => this.watchStates.notwatching);
 		}
 		if (!Array.isArray(cids) || !cids.length) {
 			return [];
 		}
 		const keys = cids.map(cid => `cid:${cid}:uid:watch:state`);
 		const [userSettings, states]: [UserSettings, (number | null)[]] = await Promise.all([
-			user.getSettings(uid),
-			db.sortedSetsScore(keys, uid),
+			user.getSettings(uid) as Promise<UserSettings>,
+			db.sortedSetsScore(keys, uid) as Promise<(number | null)[]>,
 		]);
-		return states.map(state => state || Categories.watchStates[userSettings.categoryWatchState]);
-	};
+		return states.map(state => state ?? this.watchStates[userSettings.categoryWatchState]);
+	},
 
-	Categories.getIgnorers = async function (cid: number, start: number, stop: number): Promise<number[]> {
+	getIgnorers: async function (this: CategoriesType, cid: number, start: number, stop: number): Promise<number[]> {
 		const count = (stop === -1) ? -1 : (stop - start + 1);
 		try {
-			return await db.getSortedSetRevRangeByScore(`cid:${cid}:uid:watch:state`, start, count, Categories.watchStates.ignoring, Categories.watchStates.ignoring);
-		} catch (err) {
-			console.error('Error in getIgnorers:', err);
+			const result: number[] = await db.getSortedSetRevRangeByScore(`cid:${cid}:uid:watch:state`, start, count, this.watchStates.ignoring, this.watchStates.ignoring) as number[];
+			return result;
+		} catch (err: unknown) {
+			if (err instanceof Error) {
+				console.error('Error in getIgnorers:', err.message);
+			} else {
+				console.error('Unknown error in getIgnorers');
+			}
 			return [];
 		}
-	};
+	},
 
-	Categories.filterIgnoringUids = async function (cid: number, uids: number[]): Promise<number[]> {
+	filterIgnoringUids: async function (this: CategoriesType, cid: number, uids: number[]): Promise<number[]> {
 		try {
-			const states = await Categories.getUidsWatchStates(cid, uids);
-			const readingUids = uids.filter((uid, index) => uid && states[index] !== Categories.watchStates.ignoring);
+			const states: number[] = await this.getUidsWatchStates(cid, uids);
+			const readingUids = uids.filter((uid, index) => uid && states[index] !== this.watchStates.ignoring);
 			return readingUids;
-		} catch (err) {
-			console.error('Error in filterIgnoringUids:', err);
+		} catch (err: unknown) {
+			if (err instanceof Error) {
+				console.error('Error in filterIgnoringUids:', err.message);
+			} else {
+				console.error('Unknown error in filterIgnoringUids');
+			}
 			return [];
 		}
-	};
+	},
 
-	Categories.getUidsWatchStates = async function (cid: number, uids: number[]): Promise<number[]> {
+	getUidsWatchStates: async function (this: CategoriesType, cid: number, uids: number[]): Promise<number[]> {
 		try {
 			const [userSettings, states]: [UserSettings[], (number | null)[]] = await Promise.all([
-				user.getMultipleUserSettings(uids),
-				db.sortedSetScores(`cid:${cid}:uid:watch:state`, uids),
+				user.getMultipleUserSettings(uids) as Promise<UserSettings[]>,
+				db.sortedSetScores(`cid:${cid}:uid:watch:state`, uids) as Promise<(number | null)[]>,
 			]);
-			return states.map((state, index) => state || Categories.watchStates[userSettings[index].categoryWatchState]);
-		} catch (err) {
-			console.error('Error in getUidsWatchStates:', err);
+			return states.map((state, index) => state ?? this.watchStates[userSettings[index].categoryWatchState]);
+		} catch (err: unknown) {
+			if (err instanceof Error) {
+				console.error('Error in getUidsWatchStates:', err.message);
+			} else {
+				console.error('Unknown error in getUidsWatchStates');
+			}
 			return [];
 		}
-	};
+	},
 };
+
+export default Categories;
